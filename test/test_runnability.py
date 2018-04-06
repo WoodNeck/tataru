dfaff57cd47d95a7a95eab7b274cc4dbe827e326
WoodNeck <saiyu915@naver.com>
2018-04-06 12:23:28 +0900

    fix: Fixing runnability test import dir
    - Also removing pytest cache file(and not tracking it)

--- test/test_runnability.py
+++ test/test_runnability.py
@@ -1,9 +1,9 @@
 import sys
 sys.path.append('.')
-import tataru.main
+import main
 
 
 def test_load_cog():
-    bot = tataru.main.initialize()
-    failed_cogs = tataru.main.load_cogs(bot)
+    bot = main.initialize()
+    failed_cogs = main.load_cogs(bot)
     assert(len(failed_cogs) == 0)
